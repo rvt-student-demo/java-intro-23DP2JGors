cbd73bc5eb605ff37d9d4845e7b44580aa415d57
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-10-09 09:19:57 +0000

    Dinosaur

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,8 +4,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
-
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a dinosaur");
     }
 
 }
