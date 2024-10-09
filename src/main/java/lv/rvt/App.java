b986c032daa2ef6a77d3c53d8caaf763f1e90102
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-10-09 09:47:03 +0000

    dialogue

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,12 +1,17 @@
 package lv.rvt;
 
+import java.util.Scanner;
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println("Once upon a time");
-        System.out.println("there was");
-        System.out.println("a dinosaur");
+    public static void main( String[] args ){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+
+        String message = scanner.nextLine();
+        System.out.println("Hi, "+ message + "!");
+
+        
     }
 
 }
