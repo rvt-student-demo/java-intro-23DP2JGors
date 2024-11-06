5bf4a4ab18143d7094ee575e4303e226650f352e
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-11-06 13:25:33 +0000

    Are we there yet?

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,20 +5,15 @@ public class App
 {
     public static void main( String[] args ){
         Scanner scanner = new Scanner(System.in);
-        
-        int user_num1 = scanner.nextInt(), user_num2 = scanner.nextInt();
 
-        if (user_num1 < user_num2) {
-            System.out.println(user_num1 + " ir mazāks nekā " + user_num2);
-
-        } else if(user_num1 > user_num2) {
-            System.out.println(user_num1 + " ir lielāks nekā " + user_num2);
-
-        }else {
-            System.out.println(user_num1 + " ir vienāds ar " + user_num2);
+        while (true) {
+            System.out.println("Give a number: ");
+            int user_num = scanner.nextInt();
+            if (user_num == 4) {
+                break;
+            }
         }
 
-        
     }
 
 }
