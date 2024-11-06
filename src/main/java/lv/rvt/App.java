3ca415b87f037c26f7746dc2a70ed7359dcba4e6
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-11-06 13:40:00 +0000

    Only positives

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,8 +8,16 @@ public class App
 
         while (true) {
             System.out.println("Give a number: ");
-            int user_num = scanner.nextInt();
-            if (user_num == 4) {
+            int user_input = scanner.nextInt();
+
+            if (user_input < 0) {
+                System.out.println("Unsuitable number");
+            }
+            else if (user_input > 0) {
+                user_input *= user_input;
+                System.out.println(user_input);
+            }
+            else {
                 break;
             }
         }
