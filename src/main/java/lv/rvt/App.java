4f956913cf6bd5f65eb8ed2dab3175f71f724e7e
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-11-06 10:40:56 +0000

    Comparing Numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,13 +6,16 @@ public class App
     public static void main( String[] args ){
         Scanner scanner = new Scanner(System.in);
         
-        int user_num = scanner.nextInt();
+        int user_num1 = scanner.nextInt(), user_num2 = scanner.nextInt();
 
-        if (user_num < 0) {
-            System.out.println(user_num * -1);
-        }
-        else {
-            System.out.println(user_num);
+        if (user_num1 < user_num2) {
+            System.out.println(user_num1 + " ir mazāks nekā " + user_num2);
+
+        } else if(user_num1 > user_num2) {
+            System.out.println(user_num1 + " ir lielāks nekā " + user_num2);
+
+        }else {
+            System.out.println(user_num1 + " ir vienāds ar " + user_num2);
         }
 
         
