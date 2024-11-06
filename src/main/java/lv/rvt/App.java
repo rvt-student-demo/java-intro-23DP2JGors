68a7e3a2ebcc51584363851e5dd2a1345b308e1f
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-11-06 10:29:24 +0000

    Square root of sum

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,10 +6,9 @@ public class App
     public static void main( String[] args ){
         Scanner scanner = new Scanner(System.in);
         
-        int num = scanner.nextInt();
-        
-        num *= num;
-        System.out.println(num);
+        float num1 = scanner.nextFloat(), num2 = scanner.nextFloat();
+        double square_root = Math.sqrt(num1 + num2);
+        System.out.println(square_root);
 
         
     }
