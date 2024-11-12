a7b6fbd292722225c60193bf653aa735f44643c5
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-11-12 15:57:49 +0000

    Average of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,27 +1,36 @@
 package lv.rvt;
 
 import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args ){
         Scanner scanner = new Scanner(System.in);
 
         int summa = 0;
+        int skaili = 0;
+        float av = 0;
 
-        while (true) {
-            System.out.println("Give a number: ");
+        while(true) {
+            System.out.println("Give a number:");
             int user_input = scanner.nextInt();
 
-            if (user_input == 0) {
-                System.out.println("Sum of the numbers: " + summa);
+            if(user_input == 0){
+                av = summa / skaili;
+                System.out.println("Average of the numbers: " + av);
                 break;
-            } else if (user_input != 0) {
-                summa += user_input;
             }
+            else if(user_input != 0) {
+                summa += user_input;
+                skaili++;
+            }
+        }
+
+
+        
+        }
         
             
 
     }
 
-}
-}
