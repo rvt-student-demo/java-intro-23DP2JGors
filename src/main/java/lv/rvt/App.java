7b5449699d7439fca09e7f6749f04cda5a06efea
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-11-13 09:09:08 +0000

    Counting

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,26 +7,11 @@ public class App
     public static void main( String[] args ){
         Scanner scanner = new Scanner(System.in);
 
-        int summa = 0;
-        int skaili = 0;
-        float av = 0;
+        int user_num = scanner.nextInt();
 
-        while(true) {
-            System.out.println("Give a number:");
-            int user_input = scanner.nextInt();
-
-            if(user_input == 0){
-                av = summa / skaili;
-                System.out.println("Average of the numbers: " + av);
-                break;
-            }
-            else if(user_input != 0) {
-                summa += user_input;
-                skaili++;
-            }
+        for(int i = 0; i <= user_num; i++){
+            System.out.println(i);
         }
-
-
         
         }
         
