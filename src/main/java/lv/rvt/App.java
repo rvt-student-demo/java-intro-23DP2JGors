f64ab66813c5c496a56ffbca506211e3f02484d4
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-11-13 09:13:59 +0000

    From where to where?

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,15 +7,18 @@ public class App
     public static void main( String[] args ){
         Scanner scanner = new Scanner(System.in);
 
-        int user_num = scanner.nextInt();
+        System.out.print("Where from? ");
+        int user_from = scanner.nextInt();
 
-        for(int i = user_num; i <= 100; i++){
+        System.out.print("Where to? ");
+        int user_to = scanner.nextInt();
+
+        for(int i = user_from; i <= user_to; i++){
             System.out.println(i);
-        }
-        
         }
         
             
 
     }
+}
 
