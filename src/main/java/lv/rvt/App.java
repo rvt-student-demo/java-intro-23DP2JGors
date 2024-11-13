72f1f37480a059493a77a018cadca817b9c3c740
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-11-13 10:48:25 +0000

    Reprint

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,9 +1,16 @@
 package lv.rvt;
-
+import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ){
-        printText();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many times? ");
+        int user_unput = scanner.nextInt();
+
+        for(int i = 0; i < user_unput; i++){
+            printText();
+        }
     }
 
     public static void printText() {
