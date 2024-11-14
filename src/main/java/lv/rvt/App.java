1138a31016c0da13ee53fa01e55b880314ed34d9
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-11-14 08:46:16 +0000

    From parameter to one

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ){
-        printUntilNumber(5);
-}
-
-        public static void printUntilNumber(int number) {
-        for (int i = 1; i <= number; i++) {
+        count(5);
+    } 
+    public static void count(int fromNumberToOne) {
+        for (int i = fromNumberToOne; i >= 1; i--) {
             System.out.println(i);
         }
-    } 
+    }
 }
 
