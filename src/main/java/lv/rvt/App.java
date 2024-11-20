2721a0959ead6bc7bca36964dee347475e31441e
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-11-20 11:02:55 +0000

    Number uno

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,14 +3,11 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ){
-        number(3, 10);
+        
     } 
-    public static void number(int begining, float end) {
-        for (int i = begining; i <= end; i++) {
-            if (i % 3 == 0) {
-                System.out.println(i);
-            }
-        }
+    public static int numberUno() {
+        return 1;
     }
+    
 }
 
