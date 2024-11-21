35ee765d94b584b0dc4dc6d8926839594a53a51f
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-11-21 09:13:00 +0000

    Summation

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ){
-        
+        int answer = sum(4, 3, 6, 1);
+        System.out.println("Sum: " + answer);
     } 
-    public static String word() {
-        return "Hello";
+    public static int sum(int numberOne, int numberTwo, int numberThree, int numberFour) {
+        int summa = 0;
+        return summa = numberFour + numberThree + numberOne + numberTwo;  
     }
+
     
 }
 
