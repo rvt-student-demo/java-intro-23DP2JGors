7156a922d8ecbbe2b55525efed0d54c7fa7c7aa0
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-11-26 10:43:00 +0000

    Star sign

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,22 +3,16 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ){
-
-        int answer =  greatest(2, 7, 3);
-        System.out.println("Greatest: " + answer);
+        printStars(5);
+        printStars(3);
+        printStars(9);
     } 
-
-    public static int greatest(int number1, int number2, int number3) {
-         if (number1 > number2 && number1 > number3) {
-            return number1;
-         }
-         else if (number2 > number1 && number2 > number3) {
-            return number2;
-         }else {
-            return number3;
-         }
+    public static void printStars(int number) {
+        for(int i = 0; i <= number; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
     }
 
-    
-}
+    }
 
