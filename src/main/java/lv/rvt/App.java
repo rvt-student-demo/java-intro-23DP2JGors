cec581ba9df5e54a3a9d4abffd137a7485389c7f
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-11-26 10:49:40 +0000

    Printing a rectangle

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ){
-        printSquare(4);
+        printSquare(17, 3);
     } 
-    public static void printSquare(int size) {
-        for(int i = 0; i < size; i++) {
-            for(int u = 0; u < size; u++) {
+    public static void printSquare(int width, int lenght) {
+        for(int i = 0; i < lenght; i++) {
+            for(int u = 0; u < width; u++) {
                 System.out.print("*");
             }
             System.out.println("");
