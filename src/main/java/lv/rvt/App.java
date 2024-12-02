153d9721b7984cc898913473113f4496ae79d0e4
23DP2JGors <166109688+23DP2JGors@users.noreply.github.com>
2024-12-02 12:21:31 +0000

    Greatest in list

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,21 +5,22 @@ public class App
 {
     public static void main( String[] args ){
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> wordlist = new ArrayList<>();
+        ArrayList<Integer> numlist = new ArrayList<>();
 
         int user_input = scanner.nextInt();
-        while(user_input != -1){
-            wordlist.add(user_input);
+        while (user_input != -1) {
+            numlist.add(user_input);
             user_input = scanner.nextInt();
         }
-        System.out.print("from where? ");
-        int user_from = scanner.nextInt();
-        System.out.print("To where? ");
-        int user_to = scanner.nextInt();
 
-        for(int i = user_from; i <= user_to; i++) {
-            System.out.println(wordlist.get(i));
+        int greatest = numlist.get(0);
+
+        for (int i = 0; i < numlist.size(); i++) {
+            if (greatest < numlist.get(i)) {
+                greatest = numlist.get(i);
+            }
         }
+        System.out.println("The greatest number in list: " + greatest);
         
         
     } 
